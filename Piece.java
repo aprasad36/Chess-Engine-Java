49e6eedc1aaed9ceee709e3a4fdcfa8031d91eb2
Aditya Prasad <Aditya@Adityas-MacBook-Pro.local>
2020-05-06 16:53:32 -0400

    Testing gitignore

--- Piece.java
+++ Piece.java
@@ -4,10 +4,6 @@ public interface Piece {
 
     // Every Piece will need an isProtected boolean so that the king won't be annoying
 
-    public Piece();
-
-    public Piece(boolean color, int x, int y);
-
     public Square[] getLegalMoves();
 
     public void calculateLegalMoves();
